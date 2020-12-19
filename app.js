8f5defcebcd07dcc28ef8c59beb7f40ae342eda2
rracheev <rracheev@users.noreply.github.com>
2020-12-19 13:17:01 +0300

    v.2

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require('body-parser')
+const request = require('request');
+const app = express();
+const urlencodedParser = bodyParser.urlencoded({
+    extended: false,
+})
+const JSONParser = bodyParser.json();
+
+
+app.get("/", function (request, response) {
+    response.sendFile(__dirname + "/site/html/main.html");
+})
+
+//Можно и объединить, но пока так ---
+app.use(express.static(__dirname + '/site/css'));
+app.use(express.static(__dirname + '/site/js'));
+app.use(express.static(__dirname + '/site/im'));
+//---
+
+app.post('/search', JSONParser, function (
+    req,
+    res
+) {
+    if (!req.body) return res.sendStatus(400);
+    console.log(req.body);
+    let limit = 1;
+    let term=req.body.autor_text+"+"+req.body.song_text;
+    let media = "music"
+    let str= 'limit='+limit+'&media='+media+'&term='+term;
+    console.log("req start")
+    request('https://itunes.apple.com/search?'+str, (err, response, body) => {
+        if (err) return res.status(500).send({ message: err });
+        //console.log( body);
+        let b =JSON.parse(body)
+        let music=b.results[0]
+        //console.log( music);
+        //console.log( music.artworkUrl100);
+        res.send(music.artworkUrl100)
+    });
+})
+app.listen(3000)
---
+++ site/js/main.js
@@ -0,0 +1,32 @@
+// window.onload = function() {
+// var search_button = document.getElementById("search_ok");
+// search_button.onclick = function() {
+//     let song = document.getElementById('song_text').value;
+//     let autor = document.getElementById('autor_text').value;
+//     console.log(autor)
+//     console.log(song)
+// }
+// }
+window.onload = function() {
+    let buttonSearch = document.getElementById("id_search_ok");
+    buttonSearch.onclick = Search;
+};
+
+async function getSearch() {
+    
+    let invest = {
+        autor_text: document.getElementById("id_autor_text").value,
+        song_text: document.getElementById("id_song_text").value
+      };
+    let options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(invest)
+    };
+    let promise = await fetch('/search', options)
+    return await promise.text()
+};
+
+async function Search() {
+    document.getElementById("id_albom_img").src =await getSearch()
+};
